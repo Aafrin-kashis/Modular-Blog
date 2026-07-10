3342802017d3772d963c4cf3305f9ae4892f49e4
Aafrin-kashis <sunderc178@gmail.com>
2026-07-10 17:17:24 -0400

    Uploading my blog-manager project

---
+++ blog-manager/src/js/ui.js
@@ -0,0 +1,24 @@
+// src/js/ui.js
+import { getBlogs, deleteBlog } from './data.js';
+
+export function renderBlogList(container) {
+  container.innerHTML = '';
+  getBlogs().forEach(blog => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <h3>${blog.title}</h3>
+      <p>${blog.body}</p>
+      <button data-id="${blog.id}">Delete</button>
+    `;
+    container.appendChild(li);
+  });
+}
+
+export function bindDeleteEvents(container, onDelete) {
+  container.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+      const id = parseInt(e.target.dataset.id);
+      onDelete(id);
+    }
+  });
+}
